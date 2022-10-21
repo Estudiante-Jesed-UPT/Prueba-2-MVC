a7ecb5667ecb075f425cfbcb725b182813cad7d1
sharayram <sharay_1321124008@uptecamac.edu.mx>
2022-10-20 19:03:29 -0500

    prototipo vista

--- Prueba 2 MVC/Models/Perros.cs
+++ Prueba 2 MVC/Models/Perros.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,11 @@ namespace Prueba_2_MVC.Models
     public class Perros
     {
         public int Id { get; set; }
+        [Display(Name = "Nombre del cachorro")]
         public string Name { get; set; }
-
+        [Display(Name = "Edad del cachorro")]
         public int Edad { get; set; }
-
+        [Display(Name="Tipo de Raza")]
         public string TipoRazaID { get; set; }
 
         public virtual Razas Razas { get; set; }
